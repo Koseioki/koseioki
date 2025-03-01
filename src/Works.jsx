70730fa9c964dda69ced845653434f18e546c879
Kosei Oki <koseioki1225@gmail.com>
2025-03-01 16:13:48 +0100

    added other 3 cards

--- src/Works.jsx
+++ src/Works.jsx
@@ -1,4 +1,6 @@
 import GridCard from './GridCard';
+import PCBear from './assets/pc-bear.svg';
+import Escargo from './assets/escargo.svg';
 
 function Works() {
     return (
@@ -16,8 +18,8 @@ function Works() {
                             create better designs for everyone. I am also working to meet WCAG 2.2 compliance in each project.</p>
                     </div>
                     <div>
-                        {/* <img className="side-image" id="bear-image" src="images/pc-bear.svg"
-                alt="a drawing of a smily bear using a MacBook" /> */}
+                        <img className="side-image" id="bear-image" src={PCBear}
+                            alt="a drawing of a smily bear using a MacBook" />
                     </div>
                 </div>
 
@@ -29,19 +31,19 @@ function Works() {
                         description="Working to adhere to WCAG 2.2 guidelines while maintaining visual aesthetics and the
                 overall theme."
                         link="https://koseioki.dk/dialog-paa-tvaers/" />
-       
-       <GridCard
+
+                    <GridCard
                         title="Coop Management"
                         description="A web app that prevents food waste from supermarkets.
                 Focused on user research and testing."
                         link="https://koseioki.dk/coop-management/" />
 
-<GridCard
+                    <GridCard
                         title="MusicMania"
                         description="An e-commerce case study of a fictional online store, with a focus on general usability."
                         link="https://koseioki.dk/musicmania/" />
 
-                   
+
                 </div>
             </section>
             <hr />
@@ -56,58 +58,32 @@ function Works() {
 
                     </div>
                     <div>
-                        {/* <img className="side-image" src="images/escargo.svg"
-                alt="a drawing of an escargo wearing a pair of headphones and listening to music"> */}
+                        <img className="side-image" src={Escargo}
+                            alt="a drawing of an escargo wearing a pair of headphones and listening to music" />
                     </div>
 
                 </div>
 
 
-                {/* <div className="grid">
-            <article className="grid-card transition"
-              data-target="https://wsvn.com/entertainment/deco-drive/japans-waiwai-steel-band-to-show-off-skills-at-miami-carnival-panorama/"
-              tabindex="0">
+                <div className="grid">
 
-              <div className="grid-title">
-                <h4>Miami Carnival Panorama 2022</h4>
-                <img src="images/arrow.svg" alt="" className="arrows">
-              </div>
+                    <GridCard
+                        title="Miami Carnival Panorama 2022"
+                        description="I was invited to perform at Miami Carnival Panorama 2022 as a member of Waiwai Steel Band."
+                        link="https://wsvn.com/entertainment/deco-drive/japans-waiwai-steel-band-to-show-off-skills-at-miami-carnival-panorama/" />
 
-              <img className="work-images" src="images/miami-carnival.webp" alt="">
-              <p>I was invited to perform at Miami Carnival Panorama 2022 as a member of Waiwai Steel Band</p>
-              <a className="link-text" tabindex="-1"
-                href="https://wsvn.com/entertainment/deco-drive/japans-waiwai-steel-band-to-show-off-skills-at-miami-carnival-panorama/">Read
-                the article about Miami Carnival (external link)</a>
-            </article>
+                    <GridCard
+                        title="Orchestration Studies"
+                        description="I created several wind orchestra arrangements and scores under the name iwosushi from 2016 to 2018."
+                        link="https://soundcloud.com/verywosushi" />
 
-            <article className="grid-card transition" data-target="https://soundcloud.com/verywosushi" tabindex="0">
 
-              <div className="grid-title">
-                <h4>Orchestration Studies</h4>
-                <img src="images/arrow.svg" alt="" className="arrows">
-              </div>
+                    <GridCard
+                        title="Song 1"
+                        description="Very very good song by me."
+                        link="https://soundcloud.com/koseioki/song1" />
 
-              <img className="work-images" src="images/orchestration.webp" alt="">
-              <p>I created several wind orchestra arrangements and scores under the name iwosushi from 2016 to 2018.</p>
-              <a className="link-text" tabindex="-1" href="https://soundcloud.com/verywosushi">See all my orchestration
-                works
-                (external link)</a>
-            </article>
-
-            <article className="grid-card transition" data-target="https://soundcloud.com/koseioki/song1" tabindex="0">
-              <div className="grid-title">
-                <h4>Song 1</h4>
-
-                <img src="images/arrow.svg" alt="" className="arrows">
-              </div>
-              <img className="work-images" src="images/song1.webp" alt="">
-              <p>Very very good song by me</p>
-              <a className="link-text" tabindex="-1" href="https://soundcloud.com/koseioki/song1">Listen to Song 1
-                (external
-                link)</a>
-
-            </article>
-          </div> */}
+                </div>
             </section>
 
         </section>
