d75f1fd55e8a9126f3c0332c8bc752015d679aeb
Kosei Oki <koseioki1225@gmail.com>
2025-05-20 16:08:28 +0200

    added 2 banks

--- src/AccessibleWebComponents.jsx
+++ src/AccessibleWebComponents.jsx
@@ -29,7 +29,7 @@ function AccessibleWebComponents() {
 
     <main id="main-content">
       <h1 ref={headingRef} tabIndex="-1" className="headings">Accessible Web Components</h1>
-      <p className="paragraph">These components were created for developers at <a href="https://www.bankdata.dk" className="plain-link">Bankdata</a> to help remediate the components used on the websites of its customer local banks, such as <a href="https://www.landbobanken.dk" className="plain-link">Landbobank</a>, <a href="https://www.spks.dk" className="plain-link">Sparekassen Sjælland-Fyn</a>, <a href="https://www.skjernbank.dk" className="plain-link">Skjern Bank</a>, and <a href="https://www.djurslandsbank.dk" className="plain-link">Djurslands Bank</a>.</p>
+      <p className="paragraph">These components were created for developers at <a href="https://www.bankdata.dk" className="plain-link">Bankdata</a> to help remediate the components used on the websites or netbanks of its customer local banks, such as <a href="https://www.jyskebank.dk" className="plain-link">Jyske Bank</a>, <a href="https://www.sydbank.dk" className="plain-link">Sydbank</a>, <a href="https://www.landbobanken.dk" className="plain-link">Landbobank</a>, <a href="https://www.spks.dk" className="plain-link">Sparekassen Sjælland-Fyn</a>, <a href="https://www.skjernbank.dk" className="plain-link">Skjern Bank</a>, and <a href="https://www.djurslandsbank.dk" className="plain-link">Djurslands Bank</a>.</p>
       <p className="paragraph">Each component includes a working demo and comprehensive specification documents for developers.</p>
       
       <ul className="component-list">
