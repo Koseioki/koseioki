22f01282dfa1a840d961e565b2208e1efcd0c4e1
Kosei Oki <koseioki1225@gmail.com>
2025-07-12 18:18:37 +0200

    case card focus + added profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -18,10 +18,17 @@ function Profile() {
       <img className="profile-picture" src={Kosei} alt="" aria-hidden="true" />
       <div>
         {/* <h1 ref={headingRef} tabIndex="-1" className="headings">Kosei Oki</h1> */}
-        <h1 className="headings">Hello! I'm Kosei. <span className="pronunciation">/ˈkoʊ.seɪ/</span></h1>
-        <p>I’m an inclusive digital product designer, intersectionalist, percussionist.</p>
-        <p><a className="plain-link" href="https://www.credly.com/badges/d96c149c-63c1-4764-ba03-c04703e8abbd/linked_in_profile">IAAP Certified Professionals in Accessibility Core Competencies (CPACC)</a> certified.</p>
-        <p>I also like fruits (strawberries and bananas).</p>
+        <div className="name-container">
+          <h1 className="headings">Hello! I'm Kosei.</h1>
+          <div className="pronunciation">/ˈkoʊ.seɪ/</div>
+        </div>
+        {/* <p>I’m an inclusive digital product designer, intersectionalist, percussionist.</p> */}
+
+        <p className="paragraph">I aim to create inclusive and accessible designs within Information and Communication Technologies (ICT). I'm an <a className="plain-link" href="https://www.credly.com/badges/d96c149c-63c1-4764-ba03-c04703e8abbd/linked_in_profile">IAAP Certified Professional in Accessibility Core Competencies (CPACC).</a></p>
+        <p className="paragraph">I study intersectionality and advocate for awareness of people from various backgrounds such as race, gender, sexual orientation, social class, disability, and more.
+        </p>
+        <p className="paragraph">I play percussion and steel pan (Low Tenor and Double Guitar).</p>
+        {/* <p>I also like fruits (strawberries and bananas).</p> */}
         <h2 className="sr-only">Social Media</h2>
         <ul className="link-list paragraph">
 
@@ -32,9 +39,9 @@ function Profile() {
           </li>
           <li>
             <a className="plain-link" href="https://github.com/Koseioki">
-            <Icon icon="Github"/>GitHub</a>
+              <Icon icon="Github" />GitHub</a>
           </li>
-                    <li>
+          <li>
             <a className="plain-link" href="https://www.instagram.com/koseioki1225/">
               <Icon icon="Instagram" />Instagram
             </a>
