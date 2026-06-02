d89895df9d175dacfc3a213d7a707345abf26dc3
Kosei Oki <koseioki1225@gmail.com>
2026-06-02 17:32:37 +0200

    root change

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { HelmetProvider } from 'react-helmet-async'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
-      <BrowserRouter basename={import.meta.env.DEV ? "/" : "/koseioki/"}>
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </HelmetProvider>
